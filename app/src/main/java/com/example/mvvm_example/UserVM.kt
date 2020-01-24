b5ce26304d88e1b7eddf6d47b8851cdfc2f7d45c
Jonathan Castillo <developer.jonathanc@gmail.com>
2020-01-24 12:28:30 -0600

    add API call from VM

--- app/src/main/java/com/example/mvvm_example/UserVM.kt
+++ app/src/main/java/com/example/mvvm_example/UserVM.kt
@@ -1,15 +1,33 @@
 package com.example.mvvm_example
 
-import android.util.Log
+import androidx.databinding.ObservableField
 import androidx.databinding.ObservableInt
 import androidx.lifecycle.ViewModel
+import com.example.mvvm_example.Model.Model
 import com.example.mvvm_example.Model.Model.OnResult
 
 
 class UserVM(val btnText:ObservableInt = ObservableInt(R.string.btn_text)) : ViewModel() {
 
+    var response = ObservableField<String>()
     fun onButtonClick() {
-        Log.e("JOHN","CLIKC");
+        //isVisible.set(View.VISIBLE)
+        val urlEndpoint = "apis/get_book.php"
+        //url = http://johncastle.com.mx/
+        Model().execute(BuildConfig.url + urlEndpoint, object : OnResult {
+            override fun onSuccess(result: String?) {
+                response.set(result)
+               // isVisible.set(View.GONE)
+            }
+
+            override fun onError(error: Exception) {
+                prepareError(error)
+            }
+        })
     }
 
+    fun prepareError(error: Exception) {
+        response.set(error.toString())
+        //isVisible.set(View.GONE)
+    }
 }
