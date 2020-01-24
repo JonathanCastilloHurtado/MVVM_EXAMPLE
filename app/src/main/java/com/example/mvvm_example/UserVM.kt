0a7962b45a96f345cd95482b6f619bf666781ea3
Jonathan Castillo <developer.jonathanc@gmail.com>
2020-01-24 12:17:04 -0600

    add binding events

--- app/src/main/java/com/example/mvvm_example/UserVM.kt
+++ app/src/main/java/com/example/mvvm_example/UserVM.kt
@@ -1,10 +1,15 @@
 package com.example.mvvm_example
 
-import androidx.databinding.BaseObservable
+import android.util.Log
 import androidx.databinding.ObservableInt
 import androidx.lifecycle.ViewModel
+import com.example.mvvm_example.Model.Model.OnResult
+
 
 class UserVM(val btnText:ObservableInt = ObservableInt(R.string.btn_text)) : ViewModel() {
 
+    fun onButtonClick() {
+        Log.e("JOHN","CLIKC");
+    }
 
 }
